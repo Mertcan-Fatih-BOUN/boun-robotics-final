de474f16ce45957e65cd821ce9250fdb0422e8cc
Fatih Doğan <fatih.fd.dogan@gmail.com>
2015-12-25 16:56:27 +0200

    Movements

--- final-robot/src/Main.java
+++ final-robot/src/Main.java
@@ -1,5 +1,5 @@
-import java.io.IOException;
 
+import java.io.IOException;
 
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
@@ -14,7 +14,6 @@ import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.NXTUltrasonicSensor;
 import lejos.robotics.Color;
 import lejos.robotics.ColorAdapter;
-import lejos.robotics.LightDetectorAdaptor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.utility.Delay;
@@ -25,74 +24,59 @@ public class Main {
 	static GraphicsLCD graphicsLCD = ev3.getGraphicsLCD();
 	public static int current_phase = -1;
 	public static float currentGyroFix = 0;
-	public static int current_usonic_mode = Constants.FORWARD;
-	
+
 	static EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 	static EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
 	static EV3LargeRegulatedMotor gripperMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 	static NXTRegulatedMotor ultrasonicMotor = new NXTRegulatedMotor(MotorPort.C);
-	
-	
+
 	static NXTUltrasonicSensor ultrasonicSensorLeft = new NXTUltrasonicSensor(SensorPort.S2);
 	static NXTUltrasonicSensor ultrasonicSensorRight = new NXTUltrasonicSensor(SensorPort.S4);
 	static EV3ColorSensor lightSensor = new EV3ColorSensor(SensorPort.S3);
 	static EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S1);
-	
+
 	static ColorAdapter colorAdapter = new ColorAdapter(lightSensor);
 	static SampleProvider sampleProviderGyro = gyroSensor.getAngleAndRateMode();
 	static SampleProvider sampleProviderLeft = ultrasonicSensorLeft.getDistanceMode();
 	static SampleProvider sampleProviderRight = ultrasonicSensorRight.getDistanceMode();
-	
+
 	static DifferentialPilot pilot;
-	
-	public static void main(String[] args)  throws Exception {
+
+	static Movements movements;
+
+	public static void main(String[] args) throws Exception {
 		setPilot();
 		pilot.setTravelSpeed(100);
 		pilot.setAcceleration(50);
-		
-		
+		movements = new Movements(gyroSensor, pilot);
+
 		graphicsLCD.clear();
-		graphicsLCD.drawString("Hello World", graphicsLCD.getWidth()/2, graphicsLCD.getHeight()/2, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
-		
+		graphicsLCD.drawString("Hello World", graphicsLCD.getWidth() / 2, graphicsLCD.getHeight() / 2,
+				GraphicsLCD.VCENTER | GraphicsLCD.HCENTER);
+
 		while (Button.readButtons() != Button.ID_UP) {
 			Delay.msDelay(100);
 		}
 		current_phase = Constants.PHASE1;
-		
-		print(readForward() + "");
-		Delay.msDelay(1000);
-		print(readRight() + "");
-		Delay.msDelay(1000);
-		print(readLeft() + "");
-		Delay.msDelay(1000);
-		print(readRight() + "");
-		Delay.msDelay(1000);
-		
-		//start_(current_phase);
-		Movements.goStraight(50);
-		Movements.rotate_exact(90);
-		setGyroStabilizer(90);
-		
-		Movements.goStraight(50);
-		Movements.rotate_exact(90);
-		setGyroStabilizer(180);
-		
+		// start_(current_phase);
+		movements.goStraight(50);
+		movements.rotate_left();
 
-		Movements.goStraight(50);
-		Movements.rotate_exact(90);
-		setGyroStabilizer(270);
-		
+		movements.goStraight(50);
+		movements.rotate_left();
 
-		Movements.goStraight(50);
-		Movements.rotate_exact(90);
-		setGyroStabilizer(0);
-		/*Movements.rotate_exact(-45);
-		Movements.rotate_exact(90);
-		Movements.rotate_exact(-90);
-		*/
+		movements.goStraight(50);
+		movements.rotate_left();
+
+		movements.goStraight(50);
+		movements.rotate_left();
+		/*
+		 * Movements.rotate_exact(-45); Movements.rotate_exact(90);
+		 * Movements.rotate_exact(-90);
+		 */
 	}
-	
-	public static void setPilot() throws IOException{
+
+	public static void setPilot() throws IOException {
 		PilotProps pilotProps = new PilotProps();
 		pilotProps.setProperty(PilotProps.KEY_WHEELDIAMETER, "5.5");
 		pilotProps.setProperty(PilotProps.KEY_TRACKWIDTH, "12.1");
@@ -101,77 +85,42 @@ public class Main {
 		pilotProps.setProperty(PilotProps.KEY_REVERSE, "false");
 		pilotProps.storePersistentValues();
 		pilotProps.loadPersistentValues();
-    	
-    	
-    	float wheelDiameter = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_WHEELDIAMETER, "5.5"));
-    	float trackWidth = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_TRACKWIDTH, "12.1"));
-    	boolean reverse = Boolean.parseBoolean(pilotProps.getProperty(PilotProps.KEY_REVERSE, "false"));
-    	
-    	pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
+
+		float wheelDiameter = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_WHEELDIAMETER, "5.5"));
+		float trackWidth = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_TRACKWIDTH, "12.1"));
+		boolean reverse = Boolean.parseBoolean(pilotProps.getProperty(PilotProps.KEY_REVERSE, "false"));
+
+		pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
 	}
-	
-	public static void print(String s){
-		graphicsLCD.clear();
-		graphicsLCD.drawString(s, graphicsLCD.getWidth()/2, graphicsLCD.getHeight()/2, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
-		
+
+	public static float readLeft() {
+		float[] sample = new float[sampleProviderLeft.sampleSize()];
+		sampleProviderLeft.fetchSample(sample, 0);
+
+		float distance = sample[0];
+		return distance;
 	}
-	
-	public static float readLeft(){
-		if(current_usonic_mode != Constants.LEFT){
-			ultrasonicMotor.rotate(90);
-			current_usonic_mode = Constants.LEFT;
-		}
-		float [] sample = new float[sampleProviderLeft.sampleSize()];
-    	sampleProviderLeft.fetchSample(sample, 0);
-    	
-    	float distance = sample[0];
-    	return distance;
+
+	public static float readRight() {
+		float[] sample = new float[sampleProviderRight.sampleSize()];
+		sampleProviderRight.fetchSample(sample, 0);
+
+		float distance = sample[0];
+		return distance;
 	}
-	
-	public static float readRight(){
-		if(current_usonic_mode != Constants.FORWARD){
-			ultrasonicMotor.rotate(-90);
-			current_usonic_mode = Constants.FORWARD;
-		}
-		float [] sample = new float[sampleProviderRight.sampleSize()];
-    	sampleProviderRight.fetchSample(sample, 0);
-    	
-    	float distance = sample[0];
-    	return distance;
-	}
-	
-	public static float readForward(){
-		if(current_usonic_mode != Constants.FORWARD){
-			ultrasonicMotor.rotate(90);
-			current_usonic_mode = Constants.FORWARD;
-		}
-		float [] sample = new float[sampleProviderLeft.sampleSize()];
-    	sampleProviderLeft.fetchSample(sample, 0);
-    	
-    	float distance = sample[0];
-    	return distance;
-	}
-	
-	public static float readAngle(){
-		float [] sample = new float[sampleProviderGyro.sampleSize()];
-    	sampleProviderGyro.fetchSample(sample, 0);
-    	
-    	float angle = sample[0];
-    	graphicsLCD.clear();
-		graphicsLCD.drawString(angle + "", graphicsLCD.getWidth()/2, graphicsLCD.getHeight()/2, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
-		
-    	return angle;
-	}
-	
-	public static float[] readColor(){
+
+	public static float[] readColor() {
 		Color color = colorAdapter.getColor();
-		
-    	return new float[]{color.getRed(), color.getGreen(), color.getBlue()};
+
+		return new float[] { color.getRed(), color.getGreen(), color.getBlue() };
 	}
-	
-	public static void setGyroStabilizer(float f){
+
+	public static void setGyroStabilizer(float f) {
 		currentGyroFix = f;
 	}
-	public static float getGyroStabilizer(){return currentGyroFix;}
-		
+
+	public static float getGyroStabilizer() {
+		return currentGyroFix;
+	}
+
 }
--- final-robot/src/Movements.java
+++ final-robot/src/Movements.java
@@ -1,28 +1,89 @@
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.DifferentialPilot;
+
 public class Movements {
-	public static void goStraight(int distance){
-		if(distance <= 10){
+	private int X = 0;
+	private int Y = 0;
+	private int HEADING = 0;
+
+	private EV3GyroSensor gyroSensor;
+	private DifferentialPilot pilot;
+	private SampleProvider sampleProviderGyro;
+
+	public Movements(EV3GyroSensor gyroSensor, DifferentialPilot pilot) {
+		this.gyroSensor = gyroSensor;
+		this.pilot = pilot;
+		this.sampleProviderGyro = gyroSensor.getAngleMode();
+	}
+
+	public void goStraight(int distance) {
+		if (distance <= 10) {
 			goStraight_(distance);
-		}else{
+		} else {
 			goStraight_(10);
 			goStraight(distance - 10);
 		}
 	}
-	
-	public static void goStraight_(int distance){
-		float difference = Main.getGyroStabilizer() - Main.readAngle();
+
+	private void goStraight_(int distance) {
+		float difference = HEADING - readAngle();
 		rotate_exact(difference);
-		Main.pilot.travel(distance);
-		difference = Main.getGyroStabilizer() - Main.readAngle();
+		pilot.travel(distance);
+		difference = HEADING - readAngle();
 		rotate_exact(difference);
 	}
-	
-	public static void rotate_exact(float degree){
-		float _degree = Main.readAngle();
+
+	public void rotate_left() {
+		float _goal = HEADING + 90;
+		rotate_exact_to(_goal);
+		HEADING = HEADING + 90;
+	}
+
+	public void rotate_right() {
+		float _goal = HEADING - 90;
+		rotate_exact_to(_goal);
+		HEADING = HEADING - 90;
+	}
+
+	public void rotate_back() {
+		float _goal = HEADING - 180;
+		rotate_exact_to(_goal);
+		HEADING = HEADING - 180;
+	}
+
+	private void rotate_exact_to(float goal) {
+		float diff = goal  - readAngle();
+		while (Math.abs(diff) > 0.5) {
+			pilot.rotate(diff);
+			diff = goal  - readAngle();
+		}
+	}
+
+	private void rotate_exact(float degree) {
+		float _degree = readAngle();
 		float _goal = _degree + degree;
-		Main.pilot.rotate(degree);
-		float read_angle = Main.readAngle();
-		if(read_angle <= _goal - 0.5 || read_angle >= _goal + 0.5){
+		pilot.rotate(degree);
+		float read_angle = readAngle();
+		if (read_angle <= _goal - 0.5 || read_angle >= _goal + 0.5) {
 			rotate_exact(_goal - read_angle);
 		}
 	}
+
+	public float readAngle() {
+		float[] sample = new float[sampleProviderGyro.sampleSize()];
+		sampleProviderGyro.fetchSample(sample, 0);
+		float angle = sample[0];
+		return angle;
+	}
+
+	private float normalize(float angle) {
+		float a = angle;
+		while (a > 180)
+			a -= 360;
+		while (a < -180)
+			a += 360;
+		return a;
+	}
+
 }
